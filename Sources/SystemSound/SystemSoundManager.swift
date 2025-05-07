6748d77a9926a566a7489523e514fa09a0e7d1d9
otake <ootake1@gmail.com>
2025-05-07 21:47:43 +0900

    update: sound manager

--- Sources/SystemSound/SystemSoundManager.swift
+++ Sources/SystemSound/SystemSoundManager.swift
@@ -15,7 +15,7 @@ public class SystemSoundManager {
     private var soundID: SystemSoundID = 0
     
     public func play(_ sound: SystemSound, vibrate: Bool = false) {
-        if let url = CFBundleCopyResourceURL(CFBundleGetMainBundle(), nil, nil, nil) {
+        if let url = URL(string: sound.rawValue) {
             AudioServicesCreateSystemSoundID(url as CFURL, &soundID)
             if vibrate {
                 AudioServicesAddSystemSoundCompletion(SystemSoundID(kSystemSoundID_Vibrate), nil, nil, {_,_ in }, nil)
