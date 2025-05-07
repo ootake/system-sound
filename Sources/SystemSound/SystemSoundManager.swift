e57a7a5a3cf911160346bb1c1e51788b3c1bbefa
otake <ootake1@gmail.com>
2025-05-07 21:22:18 +0900

    add: package files

---
+++ Sources/SystemSound/SystemSoundManager.swift
@@ -0,0 +1,32 @@
+//
+//  SystemSoundManager.swift
+//  SystemSound
+//
+//  Created by otake on 2025/05/07.
+//
+
+import Foundation
+import AudioToolbox
+
+@MainActor
+public class SystemSoundManager {
+    static let shared: SystemSoundManager = SystemSoundManager()
+    
+    private var soundID: SystemSoundID = 0
+    
+    func play(_ sound: SystemSound, vibrate: Bool = false) {
+        if let url = CFBundleCopyResourceURL(CFBundleGetMainBundle(), nil, nil, nil) {
+            AudioServicesCreateSystemSoundID(url as CFURL, &soundID)
+            if vibrate {
+                AudioServicesAddSystemSoundCompletion(SystemSoundID(kSystemSoundID_Vibrate), nil, nil, {_,_ in }, nil)
+            }
+            AudioServicesPlaySystemSound(soundID)
+        } else {
+            print("error: not file")
+        }
+    }
+    
+    func vibrate() {
+        AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+}
